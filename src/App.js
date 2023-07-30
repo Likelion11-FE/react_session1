655fd65063df2cdb6ff2d90268ec5ff6fc61ff32
kimgwon <92065911+kimgwon@users.noreply.github.com>
2023-07-31 02:08:38 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>리액트를 시작해봅시다</>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/detail" element={<Detail/>}/>
+          <Route path="/about" element={<About/>}/>
+        </Routes>
+      </Router>
+
     </div>
   );
 }
---
+++ src/pages/Detail.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Detail = () => {
+    return (
+        <>
+            <p>Detail</p>
+        </>
+    )
+};
+
+export default Detail;
